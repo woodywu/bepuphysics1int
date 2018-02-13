aba9e167ff35bc69cc4583d2e5e3edb1f855bdac
Bartl <paranoidsoftwarede@gmail.com>
2018-02-13 08:40:08 +0100

    Add initial benchmarks Add FixedMath reference to demos

---
+++ BEPUbenchmark/Benchmark.cs
@@ -0,0 +1,42 @@
+﻿using BEPUphysics;
+using BEPUutilities;
+using FixMath.NET;
+using System;
+
+namespace BEPUbenchmark
+{
+	abstract class Benchmark
+	{
+		protected Space space;
+		
+		protected abstract void InitializeSpace();
+		protected void Step()
+		{
+		}
+
+		public double Run()
+		{
+			space = new Space();
+			space.ForceUpdater.Gravity = new Vector3(0, (Fix64)(-9.81m), 0);
+			space.TimeStepSettings.TimeStepDuration = 1 / 60m;
+
+			InitializeSpace();
+
+			long startTime = DateTime.Now.Ticks;
+			for (int i = 0; i < 1000; i++)
+			{
+				Step();
+				space.Update();
+			}
+
+			long runTime = (DateTime.Now.Ticks - startTime);
+
+			return (double)runTime / TimeSpan.TicksPerSecond;
+		}
+
+		public string GetName()
+		{
+			return this.GetType().Name;
+		}
+	}
+}
---
+++ BEPUbenchmark/Program.cs
@@ -0,0 +1,29 @@
+﻿using BEPUbenchmark.Benchmarks;
+using System;
+
+namespace BEPUbenchmark
+{
+	class Program
+	{
+		static Benchmark[] benchmarks = { new PyramidBenchmark() };
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Running benchmarks...\n");
+
+			double runtime = 0;
+			foreach (Benchmark b in benchmarks)
+			{
+				Console.Write(b.GetName()+"... ");
+				double time = b.Run();
+
+				Console.WriteLine(Math.Round(time, 2) + "s");
+				runtime += time;
+			}
+
+			Console.WriteLine("\nCumulative runtime: "+Math.Round(runtime, 2));
+			Console.WriteLine("\nPress enter exit");
+			Console.Read();
+		}
+	}
+}

451d44868b76543a5c5e7745a56dedc0b63cfac1
Bartl <paranoidsoftwarede@gmail.com>
2018-02-22 16:17:01 +0100

    Complete first version of Fix64 Analyzer

--- Fix64Analyzer/Fix64Analyzer/Fix64AnalyzerAnalyzer.cs
+++ Fix64Analyzer/Fix64Analyzer/Fix64AnalyzerAnalyzer.cs
@@ -10,24 +10,24 @@ using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace Fix64Analyzer
 {
-    [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class Fix64AnalyzerAnalyzer : DiagnosticAnalyzer
-    {
-        public const string DiagnosticId = "F64_NUM01";
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class Fix64AnalyzerAnalyzer : DiagnosticAnalyzer
+	{
+		public const string DiagnosticId = "F64_NUM01";
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
-        private static readonly string Title = "Fix64 Analyzer";
-        private static readonly string MessageFormat = "Numeric literal error: {0}";
+		// You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+		// See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
+		private static readonly string Title = "Fix64 Analyzer";
+		private static readonly string MessageFormat = "Numeric literal error: {0}";
 		private static readonly string Description = "Replace numeric literals with Fix64 static constants.";
-        private const string Category = "Types";
+		private const string Category = "Types";
 
-        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+		private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
 
-        public override void Initialize(AnalysisContext context)
-        {
+		public override void Initialize(AnalysisContext context)
+		{
 			//context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.)
 			context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.NumericLiteralExpression);
 		}
@@ -56,7 +56,19 @@ namespace Fix64Analyzer
 
 			}
 
-			//throw new NotImplementedException();
+			BinaryExpressionSyntax expParent = literal.Parent as BinaryExpressionSyntax;
+			if (expParent != null)
+			{
+				var otherExp = expParent.Left != literal ? expParent.Left : expParent.Right;
+				symbol = context.SemanticModel.GetSymbolInfo(otherExp).Symbol;
+
+				if (symbol != null && symbol.ContainingType.ToString() == "FixMath.NET.Fix64")
+				{
+					var diagnostic = Diagnostic.Create(Rule, literal.GetLocation(), "Numeric Literal should be replaced with static constant");
+					context.ReportDiagnostic(diagnostic);
+				}
+			}
+			//throw new NotImplementedException();			
 		}
-    }
+	}
 }
--- Fix64Analyzer/Fix64Analyzer/Fix64AnalyzerCodeFixProvider.cs
+++ Fix64Analyzer/Fix64Analyzer/Fix64AnalyzerCodeFixProvider.cs
@@ -61,18 +61,34 @@ namespace Fix64Analyzer
 			return "C"+value.ToString().ToLower().Replace('-', 'm').Replace('.', 'p').Replace("f", "");
 		}
 
-        private async Task<Solution> FixLiteralAsync(Document document, LiteralExpressionSyntax literal, CancellationToken cancellationToken)
+		private string GetStaticDefintionFromValue<T>(Optional<T> value)
+		{
+			string v = value.ToString().ToLower();
+
+			string result = v.Replace('f', 'm');
+			if (v.StartsWith("-"))
+				return "(" + result + ")";
+
+			return result;
+		}
+
+		private async Task<Solution> FixLiteralAsync(Document document, LiteralExpressionSyntax literal, CancellationToken cancellationToken)
         {
+			if (document.Name == "F64.cs")
+				return null;
 			//string 
 
 			// Get the symbol representing the type to be renamed.
 			var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
 			ISymbol symbol = semanticModel.GetSymbolInfo(literal).Symbol;
 
-			string staticName = GetStaticNameFromValue(semanticModel.GetConstantValue(literal));
+			var value = semanticModel.GetConstantValue(literal);
+			string staticName = GetStaticNameFromValue(value);
 
 			if (staticName == null)
 				return null;
+
+			string staticDefinition = GetStaticDefintionFromValue(value);
 
 			var newLiteral = SyntaxFactory.ParseExpression("F64." + staticName)
 				  .WithLeadingTrivia(literal.GetLeadingTrivia())
@@ -88,15 +104,49 @@ namespace Fix64Analyzer
 			documentEditor.ReplaceNode(literal, newLiteral);
 
 			var syntaxRoot = await document.GetSyntaxRootAsync();
-			bool hasUsing = syntaxRoot.DescendantNodes().OfType<UsingDirectiveSyntax>().Any(d => d.Name.ToFullString() == "BEPUutilities.F64");
+			bool hasUsing = syntaxRoot.DescendantNodes().OfType<UsingDirectiveSyntax>().Any(d => d.Name.ToFullString() == "BEPUutilities");
 
 			if (!hasUsing)
 			{
-				var usingF64 = SyntaxFactory.ParseExpression("using BEPUutilities.F64;");
+				var usingF64 = SyntaxFactory.UsingDirective(SyntaxFactory.IdentifierName("BEPUutilities"));
 				documentEditor.InsertAfter(syntaxRoot.DescendantNodes().OfType<UsingDirectiveSyntax>().Last(), usingF64);
 			}
-			
-			
+
+			var newSolution = documentEditor.GetChangedDocument().Project.Solution;
+
+			var BEPUutilities = newSolution.Projects.Single(proj => proj.AssemblyName == "BEPUutilities");
+			var F64 = BEPUutilities.Documents.Single(doc => doc.Name == "F64.cs");
+
+			syntaxRoot = await F64.GetSyntaxRootAsync();
+			bool constantDefined = syntaxRoot.DescendantNodes().OfType<FieldDeclarationSyntax>().Any(d => d.ToString().Contains(staticName));
+			if (!constantDefined)
+			{
+				//documentEditor = await DocumentEditor.CreateAsync(F64);
+
+				var valExp = SyntaxFactory.ParseExpression("(Fix64)" + staticDefinition);
+				var definition = SyntaxFactory.FieldDeclaration(
+					SyntaxFactory.VariableDeclaration(
+						SyntaxFactory.ParseTypeName("Fix64"),
+						SyntaxFactory.SeparatedList(new[] {
+							SyntaxFactory.VariableDeclarator(
+								SyntaxFactory.Identifier( staticName ),
+								null,
+								SyntaxFactory.EqualsValueClause( valExp )
+							)
+						})
+					)).AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword), SyntaxFactory.Token(SyntaxKind.StaticKeyword), SyntaxFactory.Token(SyntaxKind.ReadOnlyKeyword)); ;
+
+				//var definition = SyntaxFactory.ParseExpression("public static readonly Fix64 " + staticName + " = (Fix64)" + staticDefinition);
+				ClassDeclarationSyntax cls = syntaxRoot.DescendantNodes().OfType<ClassDeclarationSyntax>().Single();
+
+				var newCls = cls.WithMembers(cls.Members.Add(definition));
+				//documentEditor.InsertMembers(cls, 0, new [] { definition });
+				//documentEditor.InsertAfter(cls, definition);
+				//var newDoc = documentEditor.GetChangedDocument();
+
+				newSolution = F64.WithSyntaxRoot(syntaxRoot.ReplaceNode(cls, newCls)).Project.Solution;
+			}
+
 			//var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
 
 			// Produce a new solution that has all references to that type renamed, including the declaration.
@@ -106,7 +156,7 @@ namespace Fix64Analyzer
 
 			// Return the new solution with the now-uppercase type name.
 
-			return documentEditor.GetChangedDocument().Project.Solution;
+			return newSolution;
         }
     }
 }
